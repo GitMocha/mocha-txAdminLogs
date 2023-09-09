eda9c4c3445acbce80bb38e32338764b90d45214
Mocha <97256967+GitMocha@users.noreply.github.com>
2023-09-09 11:18:42 +0100

    Update fxmanifest.lua

--- mocha_txAdminLogs/fxmanifest.lua
+++ mocha_txAdminLogs/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'mocha_txAdminLogs'
-author 'Mocha'
+author 'Mocha (Resource Creator) / Tabarra (txAdmin Source Code)'
 version '1.0.0'
 
 server_scripts { 'server/*.lua' }
